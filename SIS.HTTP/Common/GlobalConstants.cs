f7c4062574a37012a68ed33cdb94510685fa08cf
David Todorov <dtodorov@iboris.eu>
2019-07-26 15:48:14 +0300

    added validator, constants and enums

---
+++ SIS.HTTP/Common/GlobalConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.HTTP.Common
+{
+    public static class GlobalConstants
+    {
+        public const string HTTpOneProtocolFragment = "HTTP/1.1";
+        public const string HostHeaderKey = "Host";
+        public const string HttpNewLine = "\r\n";
+    }
+}
