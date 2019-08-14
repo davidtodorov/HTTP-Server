df548411f9bd0bd7d29b8c0c8c12a1a3345d19b9
David Todorov <dtodorov@iboris.eu>
2019-08-14 17:54:20 +0300

    added exceptions

---
+++ SIS.HTTP/Excceptions/InternalServerErrorException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.HTTP.Excceptions
+{
+    public class InternalServerErrorException : Exception
+    {
+        private const string InternalServerErrorExceptionDefaultMessage = "The Server has encountered an error.";
+        public InternalServerErrorException() : this(InternalServerErrorExceptionDefaultMessage)
+        {
+
+        }
+
+        public InternalServerErrorException(string message) : base(message)
+        { 
+
+        }
+    }
+}
