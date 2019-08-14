f82ccde697d9ce55ddc9ff1ab55385508465c274
David Todorov <t_david@outlook.com>
2019-08-14 20:50:36 +0300

    Added HttpHeader Collection

---
+++ SIS.HTTP/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.HTTP.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Capitalize(this string text)
+        {
+            return char.ToUpper(text[0]) + text.Substring(1).ToLower();
+        }
+    }
+}
